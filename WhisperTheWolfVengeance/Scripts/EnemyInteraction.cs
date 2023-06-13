6f1cd4d3c8b13c02213a2a769b5023f4008b37f5
natebouchat <natebouchat@gmail.com>
2023-06-13 14:14:50 -0500

    Enemy Flash on Hit

--- WhisperTheWolfVengeance/Scripts/EnemyInteraction.cs
+++ WhisperTheWolfVengeance/Scripts/EnemyInteraction.cs
@@ -4,10 +4,13 @@ using System;
 public partial class EnemyInteraction : Area2D
 {
 	public int damage {get; set;}
+	private AnimationPlayer enemyAnimation;
 
 	public override void _Ready()
 	{
 		damage = 0;
+		enemyAnimation = GetNode<AnimationPlayer>("../EnemyAnimation");
+		enemyAnimation.Play("Idle");
 	}
 
 	public void OnCharacterEntered(Node Collider) {
@@ -21,6 +24,7 @@ public partial class EnemyInteraction : Area2D
 			else {
 				damage += 5;
 			}
+			enemyAnimation.Play("Hurt");
         }
 	}
 }
