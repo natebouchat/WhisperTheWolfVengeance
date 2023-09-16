6dc38ed1e18781cb835d75ccf3c95c2f6d3e4146
Nate Bouchat <natebouchat@gmail.com>
2023-09-16 15:53:36 -0500

    Save and Load Keybinds

--- WhisperTheWolfVengeance/Scripts/Ring.cs
+++ WhisperTheWolfVengeance/Scripts/Ring.cs
@@ -3,100 +3,100 @@ using System;
 
 public partial class Ring : CharacterBody2D
 {
-    private PlayerUI playerUI;
-    private AudioStreamPlayer ringGetSFX;
-    private Vector2 motion;
-    private Random random;
-    private bool ringHasPhysics;
-    private bool hasBounced;
-    private double recollectDelay;
+	private PlayerUI playerUI;
+	private AudioStreamPlayer ringGetSFX;
+	private Vector2 motion;
+	private Random random;
+	private bool ringHasPhysics;
+	private bool hasBounced;
+	private double recollectDelay;
 
-    public override void _Ready()
-    {  
-        playerUI = GetNode<PlayerUI>("../Whisper/PlayerUI");
-        ringGetSFX = GetNode<AudioStreamPlayer>("SFXRingGet");
-        ringGetSFX.VolumeDb = _SettingsManager.sfxVolume;
-        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Modulate = new Color(1,1,1,1);
-        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("default");
+	public override void _Ready()
+	{  
+		playerUI = GetNode<PlayerUI>("../Whisper/PlayerUI");
+		ringGetSFX = GetNode<AudioStreamPlayer>("SFXRingGet");
+		ringGetSFX.VolumeDb = _SettingsManager.sfxVolume;
+		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Modulate = new Color(1,1,1,1);
+		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("default");
 
-        motion = new Vector2(400, 400);
-        ringHasPhysics = false;
-        hasBounced = false;
-        recollectDelay = 0;
-    }
+		motion = new Vector2(400, 400);
+		ringHasPhysics = false;
+		hasBounced = false;
+		recollectDelay = 0;
+	}
 
-    public override void _Process(double delta) {
-        if(ringHasPhysics) {
-            RingPhysics(delta);
-            MoveAndSlide();
-            recollectDelay -= delta;
-        }
-    }
+	public override void _Process(double delta) {
+		if(ringHasPhysics) {
+			RingPhysics(delta);
+			MoveAndSlide();
+			recollectDelay -= delta;
+		}
+	}
 
-    public void OnRingEntered(Node Collider) {
-        if(recollectDelay <= 0) {
-            CollectRing();
-        }
-    }
+	public void OnRingEntered(Node Collider) {
+		if(recollectDelay <= 0) {
+			CollectRing();
+		}
+	}
 
-    public void DropRing(Vector2 spawnPosition) {
-        recollectDelay = 0.6;
-        this.CollisionMask = 1;
-        ringHasPhysics = true;
-        this.GlobalPosition = spawnPosition;
-        random = new Random();
-        int angle = random.Next(360);
-        motion.X = (float)(Math.Cos(angle) * 600);
-        motion.Y = (float)(Math.Sin(angle) * 600);
-        GetNode<AnimationPlayer>("AnimationPlayer").Play("DelayedDisappear"); 
-    }
+	public void DropRing(Vector2 spawnPosition) {
+		recollectDelay = 0.6;
+		this.CollisionMask = 1;
+		ringHasPhysics = true;
+		this.GlobalPosition = spawnPosition;
+		random = new Random();
+		int angle = random.Next(360);
+		motion.X = (float)(Math.Cos(angle) * 600);
+		motion.Y = (float)(Math.Sin(angle) * 600);
+		GetNode<AnimationPlayer>("AnimationPlayer").Play("DelayedDisappear"); 
+	}
 
-    private void CollectRing() {
-        //Ensure ring is not collected multiple times while deleting
-        recollectDelay = 100;
-        //Delete after audio finishes
-        GetNode<Timer>("Timer").Start();
+	private void CollectRing() {
+		//Ensure ring is not collected multiple times while deleting
+		recollectDelay = 100;
+		//Delete after audio finishes
+		GetNode<Timer>("Timer").Start();
 
-        this.Visible = false;
-        playerUI.AddRings(1);
-        if(_SettingsManager.sfxVolume > -25) {
-            ringGetSFX.Play();
-        }
-    }
+		this.Visible = false;
+		playerUI.AddRings(1);
+		if(_SettingsManager.sfxVolume > -25) {
+			ringGetSFX.Play();
+		}
+	}
 
-    private void OnTimerTimeout() {
-        this.QueueFree();
-    }
+	private void OnTimerTimeout() {
+		this.QueueFree();
+	}
 
-    private void RingPhysics(double delta) {
+	private void RingPhysics(double delta) {
 		if(!IsOnFloor()) {
 			motion.Y += (float)(delta*1200);
 			if(motion.Y > 800) {
 				motion.Y = 800;
 			}
 		}
-        else if(hasBounced == false) {
-            hasBounced = true;
-            motion.Y = (-1*motion.Y)/2;
-        }
+		else if(hasBounced == false) {
+			hasBounced = true;
+			motion.Y = (-1*motion.Y)/2;
+		}
 		else {
 			motion.Y = 0;
 		}
 
-        if(motion.X > 0) {
-            motion.X -= (float)(Math.Sqrt(delta*1500));
-            if(motion.X < 0.1) {
-                motion.X = 0;
-            }
-        }
-        else if(motion.X < 0) {
-            motion.X += (float)(Math.Sqrt(delta*1500));
-            if(motion.X > -0.1) {
-                motion.X = 0;
-            }
-        }
+		if(motion.X > 0) {
+			motion.X -= (float)(Math.Sqrt(delta*1500));
+			if(motion.X < 0.1) {
+				motion.X = 0;
+			}
+		}
+		else if(motion.X < 0) {
+			motion.X += (float)(Math.Sqrt(delta*1500));
+			if(motion.X > -0.1) {
+				motion.X = 0;
+			}
+		}
 
-        this.Velocity = motion;
+		this.Velocity = motion;
 	}
 
 }
--- WhisperTheWolfVengeance/Scripts/_SettingsManager.cs
+++ WhisperTheWolfVengeance/Scripts/_SettingsManager.cs
@@ -12,17 +12,11 @@ public partial class _SettingsManager : Node
     
     public override void _Ready()
     {
-        sfxVolume = 0;
-        musicVolume = 0;        
-		fullScreen = false;
-		legacyJump = false;
-
         LoadGame();
     }
 
 	public static void SaveSettings() {
         SaveOptions();
-
         file.Save("user://save.cfg");
 	}
 
@@ -31,20 +25,59 @@ public partial class _SettingsManager : Node
         file.SetValue("Options", "SFX Volume", sfxVolume);
         file.SetValue("Options", "Full Screen", fullScreen);
         file.SetValue("Options", "Legacy Jump", legacyJump);
+
+        SaveKeyBinds();
     }
 
-    private void LoadGame() {
+
+    private static void SaveKeyBinds() {
+        string[] allActions = new string[InputMap.GetActions().Count];
+        for(int i = 0; i < InputMap.GetActions().Count; i++) {
+            allActions[i] = InputMap.GetActions()[i];
+        }
+        Array.Sort(allActions);
+        
+        for(int i = 0; i < allActions.Length; i++) {
+            string allInputs = "";
+            var actions = InputMap.ActionGetEvents(allActions[i]);
+            foreach(var a in actions) {
+                if (a is InputEventKey key) {
+                    allInputs += "Key:" + (int)key.PhysicalKeycode + ", ";
+                }
+                else if (a is InputEventJoypadButton aButton) {
+                    allInputs += "JoypadButton:" + (int)aButton.ButtonIndex + ", ";
+                }
+                else if (a is InputEventJoypadMotion joystick) {
+                    allInputs += "JoypadMotion:" + joystick.Axis;
+                    if (allActions[i].Equals("ui_up") || allActions[i].Equals("ui_left")) {
+                        allInputs += "/-, ";
+                    }
+                    else {
+                        allInputs += "/+, ";
+                    }
+                }
+            }
+            if(allInputs.Length != 0) {
+                file.SetValue("Controls", allActions[i], allInputs);
+            }
+        }
+    }
+
+    private static void LoadGame() {
        	file = new ConfigFile();
 
         Error err = file.Load("user://save.cfg");
         if(err == Error.Ok) {
-            musicVolume = (int)(file.GetValue("Options", "Music Volume"));
-            sfxVolume = (int)(file.GetValue("Options", "SFX Volume"));
-            SetScreenMode((bool)(file.GetValue("Options", "Full Screen")));
-            legacyJump = (bool)(file.GetValue("Options", "Legacy Jump"));
+            musicVolume = (int)file.GetValue("Options", "Music Volume");
+            sfxVolume = (int)file.GetValue("Options", "SFX Volume");
+            SetScreenMode((bool)file.GetValue("Options", "Full Screen"));
+            legacyJump = (bool)file.GetValue("Options", "Legacy Jump");
+
+            ParseKeybinds();
         }
         else {
             GD.Print("Load failed. Creating default save");
+            SetDefaultSettings();
             SaveSettings();
         }  
     }
@@ -57,5 +90,49 @@ public partial class _SettingsManager : Node
         else {
             DisplayServer.WindowSetMode((DisplayServer.WindowMode)0);
         }
+    }
+
+    private static void ParseKeybinds() {
+        string[] allActions = file.GetSectionKeys("Controls");
+
+        for(int i = 0; i < allActions.Length; i++) {
+            InputMap.ActionEraseEvents(allActions[i]);
+            string[] allInputs = ((string)file.GetValue("Controls", allActions[i])).Split(", ");
+            for(int j = 0; j < allInputs.Length; j++) {
+                string[] inputAndKey = allInputs[j].Split(':');
+
+                if(inputAndKey[0].Equals("Key")) {
+                    InputEventKey aKey = new InputEventKey();
+                    aKey.PhysicalKeycode = (Key)Convert.ToInt32(inputAndKey[1]);
+                    InputMap.ActionAddEvent(allActions[i], aKey);
+                }
+                else if(inputAndKey[0].Equals("JoypadButton")) {
+                    InputEventJoypadButton newButton = new InputEventJoypadButton();
+                    newButton.ButtonIndex = (JoyButton)Convert.ToInt32(inputAndKey[1]);
+                    InputMap.ActionAddEvent(allActions[i], newButton);
+                }
+                else if(inputAndKey[0].Equals("JoypadMotion")) { 
+                    string[] axisAndDirection = inputAndKey[1].Split("/");
+                    InputEventJoypadMotion newJoyMotion = new InputEventJoypadMotion();
+                    newJoyMotion.Axis = (JoyAxis)Convert.ToInt32(axisAndDirection[0]);
+                    if(axisAndDirection[1].Equals("-")) {
+                        newJoyMotion.AxisValue = -1.0f;
+                    }
+                    else {
+                        newJoyMotion.AxisValue = 1.0f;
+                    }
+                    InputMap.ActionAddEvent(allActions[i], newJoyMotion);
+                }
+            }
+        }
+    }
+
+    private static void SetDefaultSettings() {
+        sfxVolume = 0;
+        musicVolume = 0;        
+		fullScreen = false;
+		legacyJump = false;
+
+        InputMap.LoadFromProjectSettings();
     }
 }
